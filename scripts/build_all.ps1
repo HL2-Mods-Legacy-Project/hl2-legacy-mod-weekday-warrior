74817611580a446a647346fe12b2a96abd630492
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-09 20:05:54 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Weekday_Warrior" `
+  -ModName "Weekday Warrior" `
+  -ModFolder "WeekdayWarrior" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/weekday-warrior/WeekdayWarrior-Original.zip" `
+  -ModBaseFilesUrlHash "da71e78df40a05650d773983e9d2f236a67554da6edc08464ed74da3c56a060b" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
